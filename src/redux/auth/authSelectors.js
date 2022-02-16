e00ae0b853e8cce9712a120c8c3ea7664bdc7055
Alexander Korobchenko <78229551+AlexanderKorobchenko@users.noreply.github.com>
2022-02-16 17:44:21 +0200

    Delete authSelectors.js

--- src/redux/auth/authSelectors.js
+++
@@ -1,3 +0,0 @@
-// export const authenticatedSelector = state => state.auth.token;
-// export const userNameSelector = state => state.auth.user.name;
-// export const loadingAuthSelector = state => state.auth.loading;
