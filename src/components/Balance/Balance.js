9bb8a7fa317b4a9809f545bb79ad45e544f0e444
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2022-02-19 21:48:49 +0200

    add balance value

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,10 +1,19 @@
+import { useSelector } from 'react-redux';
+
+import usersSelectors from '../../redux/users/user-selectors';
 import s from './Balance.module.css';
 
 function Balance() {
+  const user = useSelector(usersSelectors.getUser);
+  const { balance } = user;
+  const balanceToString = Math.trunc(balance).toLocaleString();
+
   return (
     <div className={s.Balance}>
       <div className={s.BalanceName}>ВАШ БАЛАНС</div>
-      <div className={s.BalanceValue}>₴ 24 000.00</div>
+      <div className={s.BalanceValue}>
+        ₴<span className={s.BalanceText}>{balanceToString}.00</span>
+      </div>
     </div>
   );
 }
