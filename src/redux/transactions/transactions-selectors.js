6b14564158c3ff4a55cbb97ac13a5c650ca18715
Vasyl-mitityk <Vipsvob@gmail.com>
2022-02-21 21:12:24 +0200

    fix

--- src/redux/transactions/transactions-selectors.js
+++ src/redux/transactions/transactions-selectors.js
@@ -1,17 +1,23 @@
 const getAllTransactions = state => state.transactions.transaction;
-const getAllTransactionsError = state => state.transactions.errorTransaction;
+const getAllTransactionsError = state => state.transactions.error;
+const getAllisFetchingTransaction = state => state.transactions.isFetchingTransaction;
 const createTransaction = state => state.transactions.result;
-const createTransactionError = state => state.transactions.errorTransaction;
+const createTransactionError = state => state.transactions.error;
+const createTransactionCreated = state => state.transactions.created;
 const getStatistics = state => state.transactions.statistic;
-const getStatisticsError = state => state.transactions.errorStatistic;
+const getStatisticsError = state => state.transactions.error;
+const getIsFetchingStatistic= state => state.transactions.isFetchingStatistic;
 
 const transactionsSelectors = {
   getAllTransactions,
   getAllTransactionsError,
+  getAllisFetchingTransaction,
   createTransaction,
   createTransactionError,
+  createTransactionCreated,
   getStatistics,
   getStatisticsError,
+  getIsFetchingStatistic,
 };
 
 export default transactionsSelectors;
